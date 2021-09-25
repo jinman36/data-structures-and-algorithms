1db3f89cb3f826fe5274a942061961a84ad9c7fe
Jeff Inman <jeffery.inman@hotmail.com>
2021-09-24 20:51:10 -0700

    stretch goal 7 complete

--- javascript/code-challenges/challenges-02.test.js
+++ javascript/code-challenges/challenges-02.test.js
@@ -96,7 +96,9 @@ For example: charCode(['h','i']) returns [104, 105].
 ------------------------------------------------------------------------------------------------ */
 
 const charCode = (arr) => {
-  // Solution code here...
+  return arr.map((index) => {
+    return index = index.charCodeAt();
+  });
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -268,7 +270,7 @@ describe('Testing challenge 6', () => {
   });
 });
 
-xdescribe('Testing challenge 7', () => {
+describe('Testing challenge 7', () => {
   test('It should return an array containing the character code for each letter', () => {
     expect(charCode(['C', 'o', 'd', 'e', '3', '0', '1'])).toStrictEqual([ 67, 111, 100, 101, 51, 48, 49 ]);
     expect(charCode(['C', 'o', 'd', 'e', '3', '0', '1']).length).toStrictEqual(7);
